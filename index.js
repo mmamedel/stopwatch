774b1107b1704d0048a5afbc25a6e84fa57f68bb
mmamedel <mmamedel@gmail.com>
2021-03-22 09:50:07 -0400

    Trying to fix mobile click

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import ReactDOM from 'https://cdn.skypack.dev/react-dom?dts'
 import { html } from './helpers/react.js'
 import { RecoilRoot } from 'https://cdn.skypack.dev/recoil?dts'
 import App from './components/app/index.js'
+import fastclick from 'https://cdn.skypack.dev/fastclick';
 
 ReactDOM.render(html`<${RecoilRoot}><${App} /><//>`, document.body)
+fastclick(document.body)
